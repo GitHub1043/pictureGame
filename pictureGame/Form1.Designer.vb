2908136db01e77e1f7fced168f001f8f3b834b4f
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:27:48 -0600

    Initial commit

---
+++ pictureGame/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picImage = New System.Windows.Forms.PictureBox()
+        Me.radOption1 = New System.Windows.Forms.RadioButton()
+        Me.radOption2 = New System.Windows.Forms.RadioButton()
+        Me.radOption3 = New System.Windows.Forms.RadioButton()
+        Me.radOption4 = New System.Windows.Forms.RadioButton()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        CType(Me.picImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picImage
+        '
+        Me.picImage.Location = New System.Drawing.Point(319, 20)
+        Me.picImage.Name = "picImage"
+        Me.picImage.Size = New System.Drawing.Size(522, 245)
+        Me.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picImage.TabIndex = 0
+        Me.picImage.TabStop = False
+        '
+        'radOption1
+        '
+        Me.radOption1.AutoSize = True
+        Me.radOption1.Location = New System.Drawing.Point(251, 291)
+        Me.radOption1.Name = "radOption1"
+        Me.radOption1.Size = New System.Drawing.Size(65, 17)
+        Me.radOption1.TabIndex = 1
+        Me.radOption1.TabStop = True
+        Me.radOption1.Text = "Option 1"
+        Me.radOption1.UseVisualStyleBackColor = True
+        '
+        'radOption2
+        '
+        Me.radOption2.AutoSize = True
+        Me.radOption2.Location = New System.Drawing.Point(435, 291)
+        Me.radOption2.Name = "radOption2"
+        Me.radOption2.Size = New System.Drawing.Size(65, 17)
+        Me.radOption2.TabIndex = 2
+        Me.radOption2.TabStop = True
+        Me.radOption2.Text = "Option 2"
+        Me.radOption2.UseVisualStyleBackColor = True
+        '
+        'radOption3
+        '
+        Me.radOption3.AutoSize = True
+        Me.radOption3.Location = New System.Drawing.Point(619, 291)
+        Me.radOption3.Name = "radOption3"
+        Me.radOption3.Size = New System.Drawing.Size(65, 17)
+        Me.radOption3.TabIndex = 3
+        Me.radOption3.TabStop = True
+        Me.radOption3.Text = "Option 3"
+        Me.radOption3.UseVisualStyleBackColor = True
+        '
+        'radOption4
+        '
+        Me.radOption4.AutoSize = True
+        Me.radOption4.Location = New System.Drawing.Point(803, 291)
+        Me.radOption4.Name = "radOption4"
+        Me.radOption4.Size = New System.Drawing.Size(65, 17)
+        Me.radOption4.TabIndex = 4
+        Me.radOption4.TabStop = True
+        Me.radOption4.Text = "Option 4"
+        Me.radOption4.UseVisualStyleBackColor = True
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(543, 395)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheck.TabIndex = 5
+        Me.btnCheck.Text = "Check Guess"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1160, 450)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.radOption4)
+        Me.Controls.Add(Me.radOption3)
+        Me.Controls.Add(Me.radOption2)
+        Me.Controls.Add(Me.radOption1)
+        Me.Controls.Add(Me.picImage)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picImage As System.Windows.Forms.PictureBox
+    Friend WithEvents radOption1 As System.Windows.Forms.RadioButton
+    Friend WithEvents radOption2 As System.Windows.Forms.RadioButton
+    Friend WithEvents radOption3 As System.Windows.Forms.RadioButton
+    Friend WithEvents radOption4 As System.Windows.Forms.RadioButton
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+
+End Class
---
+++ pictureGame/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Dim intPicture As Integer
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        If intPicture = 1 And radOption1.Checked Then
+            MessageBox.Show("Correct")
+        Else
+            MessageBox.Show("Wrong")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        intPicture = 2
+
+
+        If intPicture = 1 Then
+            picImage.Image = My.Resources.australia
+            radOption1.Text = "Australia"
+            radOption2.Text = "Costa Rica"
+            radOption3.Text = "USA"
+            radOption4.Text = "Italy"
+        End If
+
+        If intPicture = 2 Then
+            picImage.Image = My.Resources.canada
+            radOption1.Text = "USA"
+            radOption2.Text = "Germany"
+            radOption3.Text = "Canada"
+            radOption4.Text = "England"
+        End If
+
+        If intPicture = 3 Then
+            picImage.Image = My.Resources.costaRica
+            radOption1.Text = "England"
+            radOption2.Text = "Costa Rica"
+            radOption3.Text = "Australia"
+            radOption4.Text = "USA"
+        End If
+
+        If intPicture = 4 Then
+            picImage.Image = My.Resources.england
+            radOption1.Text = "England"
+            radOption2.Text = "Germany"
+            radOption3.Text = "USA"
+            radOption4.Text = "Canada"
+        End If
+
+        If intPicture = 5 Then
+            picImage.Image = My.Resources.germany
+            radOption1.Text = "Greece"
+            radOption2.Text = "Italy"
+            radOption3.Text = "Germany"
+            radOption4.Text = "Australia"
+        End If
+
+        If intPicture = 6 Then
+            picImage.Image = My.Resources.greece
+            radOption1.Text = "Greece"
+            radOption2.Text = "Italy"
+            radOption3.Text = "Australia"
+            radOption4.Text = "USA"
+        End If
+
+        If intPicture = 7 Then
+            picImage.Image = My.Resources.italy
+            radOption1.Text = "Germany"
+            radOption2.Text = "Costa Rica"
+            radOption3.Text = "Canada"
+            radOption4.Text = "Italy"
+        End If
+
+        If intPicture = 8 Then
+            picImage.Image = My.Resources.usa
+            radOption1.Text = "Canada"
+            radOption2.Text = "USA"
+            radOption3.Text = "Germany"
+            radOption4.Text = "England"
+        End If
+
+    End Sub
+End Class
